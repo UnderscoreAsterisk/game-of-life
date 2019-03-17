87403a463cf82d3ec699663928e21f50857b7158
unknown <apoorvgoel_bt2k16@dtu.ac.in>
2019-03-17 18:56:15 +0530

    Implement backend

--- gol.py
+++ gol.py
@@ -1,26 +1,61 @@
 import scipy.signal as signal
 import numpy as np
+import json
 
-kernel = np.ones((3,3), dtype=np.uint8)
-init_board = np.array([[0,0,0,0,0],[0,1,1,1,0],[0,0,0,0,0]], dtype=np.uint8)
+CONFIG_PATH = "config.json"
 
-def update(board):
-    old_board = board
-    new_board = signal.convolve2d(old_board, kernel, mode="same")
+class GameOfLife:
+    def __init__(self):
+        with open(CONFIG_PATH) as f:
+            json_str = f.read()
+        
+        self.config = json.loads(json_str)
+        
+        self.cur_board = None
+        
+        #Initialise Stats
+        self.num_iters = 0
+        self.num_alive = 0
+        self.pattern_name = None
 
-    (m, n) = old_board.shape
-    for i in range(m):
-        for j in range(n):
-            if new_board[i,j] == 3:
-                new_board[i, j] = 1
-            elif new_board[i, j] == 4:
-                new_board[i, j] = old_board[i, j]
-            else:
-                new_board[i, j] = 0
+    def get_options(self):
+        return self.config
 
-    print(new_board)
-    return new_board
+    def select(self, pattern_name):
+        self.pattern_name = pattern_name
+        if pattern_name.lower() not in self.config:
+            pass #Raise error for invalid selection
+        else:
+            self.cur_board = np.array(self.config[pattern_name.lower()], dtype=np.uint8)
 
-print(init_board)
-new_board = update(init_board)
-new_board = update(new_board)
+    def update(self):
+        kernel = np.ones((3,3), dtype=np.uint8)
+        if self.cur_board is None:
+            pass #Raise error that no option is chosen
+
+        new_board = signal.convolve2d(self.cur_board, kernel, mode="same")
+
+        (m, n) = self.cur_board.shape
+        for i in range(m):
+            for j in range(n):
+                if new_board[i,j] == 3:
+                    new_board[i, j] = 1
+                elif new_board[i, j] == 4:
+                    new_board[i, j] = self.cur_board[i, j]
+                else:
+                    new_board[i, j] = 0
+
+        self.cur_board = new_board
+        self.num_iters += 1
+        self.num_alive = sum(sum(self.cur_board))
+
+    def get_board(self):
+        return self.cur_board
+
+    def get_stats(self):
+        d = {
+            "pattern": self.pattern_name,
+            "alive": self.num_alive,
+            "iters": self.num_iters
+        }
+        return d
